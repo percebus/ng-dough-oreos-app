1159b7a640bb4579f4ca7ce0c02760aa865b978b
JCGuerrero <JCystems@gMail.com>
2025-04-11 13:57:09 -0500

    chore(ng update): use-application-builder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 4200;
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist/browser"));
 app.listen(port, () => {
   console.log(`Server started at ${port}`, process.env);
 });

b430831d046edf58d3dee55f2ffdac39bd4e4a7d
JCGuerrero <JCystems@gMail.com>
2025-04-11 15:13:50 -0500

    chore(Swashbuckle): Added swagger

--- dotnet/WebApp/Program.cs
+++ dotnet/WebApp/Program.cs
@@ -19,6 +19,7 @@ namespace WebApp
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Swagger"));
             }
 
             app.UseHttpsRedirection();
